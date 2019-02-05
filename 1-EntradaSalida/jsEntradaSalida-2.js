a9866904e4300ee32ab38071551cfdbb59279300
alumno <alumno>
2019-02-04 21:43:42 -0300

    Se agrega como usar git + lo que hice en clase

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,27 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	//Esto es un comentario en JavaScript
+	/*
+	Esto es un comenmtario de bloque en JS
+	*/
+
+	//Guardo en la variable nomre lo que se ingresa por prompt "Harry Potter" es un predeterminado que se muestra 
+	var person = prompt("Please enter your name", "Harry Potter");
+	//Muestro lo que se Ingreso en la variable
+	alert(person);
+
+	/*
+	Otra forma de hacerlo
+	*/
+	//Primero Declaro la variable
+	var nombre;
+	//Luego inicializo la variable y guardo lo que se ingresa por prompt
+	nombre=prompt("Por favor Ingresa tu nombre");
+	//Muestro lo que se guardo en la variable
+	alert(nombre);
+	//Si el contenido del alert tiene "" se escribe el literal, no es la variable
+	alert("nombre");
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,9 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	//Tomo el value de un input text, lo guardo en una variable y lo muestro
+	var nombre = elNombre.value;
+	alert(nombre);
 }
 
 
