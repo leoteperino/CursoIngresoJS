450d6dbc2415f344afb3db758e5c7a4ca019eb7a
unknown <leoteperino@gmail.com>
2019-02-10 20:30:23 -0300

    Ejercicios del 1 al 3 Parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,19 @@
 function mostrar()
 {
+	var ingresaPrecio;
+	var ingresaPorcentaje;
+	var porcentajeIngresado;
+	var precioFinal;
 
+	ingresaPrecio = prompt("Ingrese el Precio");
+	ingresaPorcentaje = prompt("Ingrese el porcentaje de descuento");
+
+	ingresaPrecio = parseInt(ingresaPrecio);
+	ingresaPorcentaje = parseInt(ingresaPorcentaje);
+
+	porcentajeIngresado = 1 - ingresaPorcentaje / 100;
+
+	precioFinal = ingresaPrecio * porcentajeIngresado;
+
+	elPrecioFinal.value = precioFinal;
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,17 @@
 
 function mostrar()
 {
+	var largo;
+	var ancho;
+	var perimetro;
 
+	largo = prompt("Ingrese el largo de un Rectangulo");
+	ancho = prompt("Ingrese el ancho de un Rectangulo");
+
+	largo = parseInt(largo);
+	ancho = parseInt(ancho);
+
+	perimetro = 2 * (largo + ancho);
+
+	alert ("El perimetro del Rectangulo es: " + perimetro + "cm");
 }
