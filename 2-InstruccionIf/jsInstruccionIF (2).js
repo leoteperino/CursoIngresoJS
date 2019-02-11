854c582822abbe123cb23441250a808260c13cd7
alumno <alumno>
2019-02-11 19:47:33 -0300

    Instruccion IF - 2

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,7 +1,14 @@
 function mostrar()
 {
 //tomo la edad  
+	//tomo la edad
+	var edadIngresada;
 
+	edadIngresada = edad.value;
 
+	edadIngresada = parseInt(edadIngresada);
 
+	if (edadIngresada > 17){	
+		alert("Es mayor de edad");
+	} 
 }//FIN DE LA FUNCIÓN
