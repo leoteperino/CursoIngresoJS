327b2d48d107232039f7542e58ab2fcd62165fb3
unknown <leoteperino@gmail.com>
2019-02-10 21:13:09 -0300

    Ejercicios del 1 al 3 Recuperatorios

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,39 @@
 function mostrar()
 {
+	var precioIngresado;
+	var porcentajeIngresado;
+	var calculoDescuentoEnDinero;
+	var descuentoEnDinero;
+	var calculoPrecioConDescuento;
+	var precioConDescuento;
+	var calcularIva;
+	var ivaMostrar;
+	var precioFinal;
 
+	precioIngresado = prompt("Ingrese un precio");
+	porcentajeIngresado = prompt ("Ingrese el porcentaje de descuento");
+
+	precioIngresado = parseInt(precioIngresado);
+	porcentajeIngresado = parseInt(porcentajeIngresado);
+
+	calculoDescuentoEnDinero = porcentajeIngresado / 100;
+
+	descuentoEnDinero = precioIngresado * calculoDescuentoEnDinero;
+
+	calculoPrecioConDescuento = 1 - (porcentajeIngresado / 100);
+
+	precioConDescuento = precioIngresado * calculoPrecioConDescuento;
+
+	calcularIva = precioConDescuento * 1.21;
+
+	ivaMostrar = "21%";
+
+	alert("El descuento en dinero es: " + descuentoEnDinero + "\n" + 
+		  "El precio con el descuento es: " + precioConDescuento + "\n" +
+		  "El IVA es del: " + ivaMostrar + "\n" +
+		  "Al dar click en OK aparecera el precio Final con descuento + IVA en el recuadro.");
+
+	precioFinal = calcularIva;
+
+	elPrecioFinal.value = precioFinal;
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,19 @@
 
 function mostrar()
 {
+	var base;
+	var altura;
+	var superficie;
+	var perimetro;
 
+	base = prompt("Ingrese la base");
+	altura = prompt("Ingrese la altura");
+
+	base = parseInt(base);
+	altura = parseInt(altura);
+
+	superficie = base * 3;
+	perimetro = (base * altura) / 2;
+
+	alert("La base del triangulo es: " + base + " y su superficie es: " + superficie);
 }
