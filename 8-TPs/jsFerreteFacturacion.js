7bfcf9ff71258beae7279753110dafb89c3d82b7
unknown <leoteperino@gmail.com>
2019-02-10 23:38:56 -0300

    TP 1 - 1

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,63 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var primerPrecioIngresado;
+	var segundoPrecioIngresado;
+	var tercerPrecioIngresado;
+	var sumaPrecios;
+
+	primerPrecioIngresado = PrecioUno.value;
+	segundoPrecioIngresado = PrecioDos.value;
+	tercerPrecioIngresado = PrecioTres.value;
+
+	primerPrecioIngresado = parseInt(primerPrecioIngresado);
+	segundoPrecioIngresado = parseInt(segundoPrecioIngresado);
+	tercerPrecioIngresado = parseInt(tercerPrecioIngresado);
+
+	sumaPrecios = primerPrecioIngresado + segundoPrecioIngresado + tercerPrecioIngresado;
+
+	alert("La suma de los precios es: $" + sumaPrecios);	
 }
+
 function Promedio () 
 {
-	
+	var primerPrecioIngresado;
+	var segundoPrecioIngresado;
+	var tercerPrecioIngresado;
+	var promedio;
+
+	primerPrecioIngresado = PrecioUno.value;
+	segundoPrecioIngresado = PrecioDos.value;
+	tercerPrecioIngresado = PrecioTres.value;
+
+	primerPrecioIngresado = parseInt(primerPrecioIngresado);
+	segundoPrecioIngresado = parseInt(segundoPrecioIngresado);
+	tercerPrecioIngresado = parseInt(tercerPrecioIngresado);
+
+	promedio =  (primerPrecioIngresado + segundoPrecioIngresado + tercerPrecioIngresado) / 3;
+
+	alert("El promedio de los precios es: $" + promedio);
 }
+
 function PrecioFinal () 
 {
-	
+	var primerPrecioIngresado;
+	var segundoPrecioIngresado;
+	var tercerPrecioIngresado;
+	var iva;
+	var ivaCalculado;
+
+	primerPrecioIngresado = PrecioUno.value;
+	segundoPrecioIngresado = PrecioDos.value;
+	tercerPrecioIngresado = PrecioTres.value;
+
+	primerPrecioIngresado = parseInt(primerPrecioIngresado);
+	segundoPrecioIngresado = parseInt(segundoPrecioIngresado);
+	tercerPrecioIngresado = parseInt(tercerPrecioIngresado);
+
+	iva = 1.21;
+
+	ivaCalculado =  (primerPrecioIngresado + segundoPrecioIngresado + tercerPrecioIngresado) * iva;
+
+	alert("El precio final con el iva calculado es: $" + ivaCalculado);
 }
