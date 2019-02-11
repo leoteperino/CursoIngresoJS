952e9ca79498fbaf67d782432c90099fa2ade33e
alumno <alumno>
2019-02-11 19:28:43 -0300

    Instruccion IF - 1

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,13 @@
 function mostrar()
 {
-//tomo la edad  
+//tomo la edad
+var edadIngresada;
 
+edadIngresada = edad.value;
 
+edadIngresada = parseInt(edadIngresada);
 
+if (edadIngresada == 15){
+	alert("Niña Bonita");
+	} 
 }//FIN DE LA FUNCIÓN
