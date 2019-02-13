6db7f1517313c2c5efecd35b084714a09726679e
unknown <leoteperino@gmail.com>
2019-02-13 00:49:12 -0300

    Ejercicio 9 y 10

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,16 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	
+	var notaRandom;
+	//Genero el número RANDOM entre 1 y 10
+	notaRandom = Math.floor((Math.random() * 10) + 1);
 
+	if(notaRandom > 8){
+		alert("EXCELENTE" + "\n" + "Su nota fue un: " + notaRandom);
+	}else{
+		if(notaRandom > 3){
+			alert("Aprobo" + "\n" + "Su nota fue un: " + notaRandom);
+		}else{
+			alert("Vamos la proxima se puede" + "\n" + "Su nota fue un: " + notaRandom);
+		}
+	}
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,8 @@
 function mostrar()
 {
+	var numeroRandom;
 	//Genero el número RANDOM entre 1 y 10 
-	
+	numeroRandom = Math.floor((Math.random() * 10) + 1);
 
+	alert(numeroRandom);
 }//FIN DE LA FUNCIÓN
