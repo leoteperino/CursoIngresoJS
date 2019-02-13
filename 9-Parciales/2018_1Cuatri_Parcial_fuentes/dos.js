0e706af742b6aa433df2da12b1c89bb0e7d91638
alumno <alumno>
2019-02-13 19:42:00 -0300

    Ejercicio 2

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,10 +1,28 @@
 function mostrar()
 {
-	var nombre;
+	/*var nombre;
 	var localidad;
 
 	nombre = elNombre.value;
 	localidad = laLocalidad.value;
 
-	alert("Usted es " + nombre + " y vive en la localidad de " + localidad);  
+	alert("Usted es " + nombre + " y vive en la localidad de " + localidad);  */
+
+	//Ejercicio 2
+	var ingresaPrecio;
+	var porcentaje;
+	var precioFinal;
+
+	ingresaPrecio = prompt("Ingrese el Precio");
+
+	ingresaPrecio = parseInt(ingresaPrecio);
+	
+	porcentaje = (ingresaPrecio * 10) / 100;
+
+	precioFinal = ingresaPrecio + porcentaje;
+
+	alert("El precio en efectivo es: $" + ingresaPrecio + "\n" + 
+		  "Con tarjeta ha y un recargo del 10% que es: $" + porcentaje + "\n" +
+		  "El precio final del producto es: $" + precioFinal);
+
 }
