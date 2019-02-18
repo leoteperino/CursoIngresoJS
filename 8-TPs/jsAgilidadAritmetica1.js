f4d29172b5bca8de96224ef35fb00d53b277e783
unknown <leoteperino@gmail.com>
2019-02-17 23:11:37 -0300

    Agilidad Aritmetica 1

--- 8-TPs/jsAgilidadAritmetica1.js
+++ 8-TPs/jsAgilidadAritmetica1.js
@@ -7,14 +7,36 @@ y una de las cuatro operaciones básicas (suma, resta,
 se debe informar si el resultado es el correcto o no.
 */
 var respuesta;
+var operadoresLogicos;
+var primerNumero;
+var segundoNumero;
+
 function comenzar()
 {
+	operadoresLogicos = new Array('1','2','3','4');
+	operadoresLogicos = operadoresLogicos[Math.floor(Math.random()*operadoresLogicos.length)];
 
-	
+	primerNumero = parseInt(primerNumero);
+	segundoNumero = parseInt(segundoNumero);
 
+	primerNumero = Math.floor((Math.random() * 10) + 1);
+	segundoNumero =Math.floor((Math.random() * 10) + 1);
+
+	PrimerNumero.value = primerNumero;
+	SegundoNumero.value = segundoNumero;
+
+	if(operadoresLogicos == 1){
+		Operador.value = +;
+	}
+	if(operadoresLogicos == 2){
+		Operador.value = -;
+	}
+	if(operadoresLogicos == 3){
+		Operador.value = *;
+	}
 }//FIN DE LA FUNCIÓN
+
 function Responder()
 {
-	
-
+	alert(respuesta);
 }//FIN DE LA FUNCIÓN
