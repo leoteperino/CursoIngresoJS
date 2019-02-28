e9b3d823183074ac78e7549691d55132d763f810
alumno <alumno>
2019-02-27 22:12:47 -0300

    Clase del dia 27-2-2019

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -10,11 +10,70 @@ function mostrar()
 			console.log(contador);	
 		}
 	============*/
-	var contador=0;
-	for(  ; contador<10 ; ){
-		console.log(contador);
-		contador++;
+	
+	var contador;
+	var numeroRandom;
+	var contador1 = 0;
+	var contador2 = 0;
+	var contador3 = 0;
+	var contador4 = 0;
+	var contador5 = 0;
+	var contador6 = 0;
+	var contador7 = 0;
+	var contador8 = 0;
+	var contador9 = 0;
+	var contador10 = 0;
+
+	for(contador=0; contador<100; contador++){
+		numeroRandom = Math.floor((Math.random() * 10) + 1);
+		switch(numeroRandom){
+			case 1:
+				contador1++;
+				break;
+			case 2:
+				contador2++;
+				break;
+			case 3:
+				contador3++;
+				break;
+			case 4:
+				contador4++;
+				break;
+			case 5:
+				contador5++;
+				break;
+			case 6:
+				contador6++;
+				break;
+			case 7:
+				contador7++;
+				break;
+			case 8:
+				contador8++;
+				break;
+			case 9:
+				contador9++;
+				break;
+			case 10:
+				contador10++;
+				break;
+		}
 	}
-	console.log(contador);
+
+	console.log("El 1 salio " + contador1 + " veces y es el " + (contador1*100)/contador + "% de las veces");
+	console.log("El 2 salio " + contador2 + " veces y es el " + (contador2*100)/contador + "% de las veces");
+	console.log("El 3 salio " + contador3 + " veces y es el " + (contador3*100)/contador + "% de las veces");
+	console.log("El 4 salio " + contador4 + " veces y es el " + (contador4*100)/contador  + "% de las veces");
+	console.log("El 5 salio " + contador5 + " veces y es el " + (contador5*100)/contador  + "% de las veces");
+	console.log("El 6 salio " + contador6 + " veces y es el " + (contador6*100)/contador  + "% de las veces");
+	console.log("El 7 salio " + contador7 + " veces y es el " + (contador7*100)/contador  + "% de las veces");
+	console.log("El 8 salio " + contador8 + " veces y es el " + (contador8*100)/contador  + "% de las veces");
+	console.log("El 9 salio " + contador9 + " veces y es el " + (contador9*100)/contador  + "% de las veces");
+	console.log("El 10 salio " + contador10 + " veces y es el " + (contador10*100)/contador  + "% de las veces");
+
+	var palabra = "Luz Azul";
+	for(i=0;i<palabra.length;i++){
+		console.log(palabra[i]);	
+	}
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
@@ -1,4 +1,29 @@
 function mostrar()
 {
+		var nombreIngresado;
+		var pesoIngresado;
+		var temperaturaIngresada;
+		var respuesta;
+		var seguirIngresando;
+
+		respuesta = "si"; 
+
+		while(respuesta == "si"){
+			nombreIngresado = prompt("Ingrese el nombre de un animal: ");
+			while(!isNaN(nombreIngresado)){
+				nombreIngresado = prompt("ERROR. Le dije que Ingrese un nombre, no un numero");
+			}
+			pesoIngresado = prompt("Ingrese el peso, debe ser entre 1 y 1000: ");
+			pesoIngresado = parseInt(pesoIngresado);
+			while(pesoIngresado == 0 || pesoIngresado > 1000){
+				pesoIngresado = prompt("ERROR. le dije entre 1 y 1000, Ingreselo nuevamente: ");
+			}
+			temperaturaIngresada = prompt("Ingrese la temperatura entre -30 y 30:"); 
+			respuesta = prompt("Desea seguir ingresando animales? si o no?")
+			
+		}
+
+
+
 
 }
